5847eb21a7478976a51bffeae73236b87a010db5
Morgana Bellacci <moghymorghy@gmail.com>
2019-03-09 17:09:25 +0100

    Inserito metodo che calcola il Fattoriale.

---
+++ CalcoliCombinatori.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcoliCombinatori.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("CalcoliCombinatori.Test")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6314048a-4c35-42ad-9128-afa155632431")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EquazioniLibrary/CalcoliCombinatori.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquazioniLibrary
+{
+    public class CalcoliCombinatori
+    {
+        public static int Fattoriale(int n)
+        {
+            //inizializzo le variabile di ritorno
+            int fattoriale = 1;
+
+            //inserisco un for per fare il calcolo del fattoriale
+            for (int i = 1; i <= n; i = i + 1)
+            {
+                fattoriale = fattoriale * i;
+            }
+
+            return fattoriale;
+        }
+    }
+}
